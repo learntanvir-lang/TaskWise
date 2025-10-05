900f709701a5cbd5c356eca2019bd9a885c2be32
Tanvir Mahmud <learntanvir@gmail.com>
2025-10-05 10:31:04 +0000

    make it the favicon (_for element <Logo>_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="system"
             enableSystem
+            disableTransitionOnChange={false}
         >
             <AuthProvider>
             {children}
