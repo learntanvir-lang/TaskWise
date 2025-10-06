a4346bab8aa9c4ec9a1cc72aca8511bca87055dd
Tanvir Mahmud <learntanvir@gmail.com>
2025-10-06 05:57:49 +0000

    why i can not use the app offline?

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,10 @@
+// next.config.ts
 import type {NextConfig} from 'next';
+
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -32,4 +38,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
